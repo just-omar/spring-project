0b570d08ad991f1455768317dd2e6f569669a586
omar <medzhidov.omar11@gmail.com>
2026-06-14 12:09:00 +0300

    first commit

---
+++ src/main/java/com/example/demo/Blah.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+public class Blah {
+    private String blahify;
+
+    public String getBlahify() {
+        return blahify;
+    }
+    public void setBlahify(String blahify) {
+
+    }
+
+
+}
---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.kafka.autoconfigure.KafkaAutoConfiguration;
+import org.springframework.boot.liquibase.autoconfigure.LiquibaseAutoConfiguration;
+import org.springframework.boot.security.autoconfigure.SecurityAutoConfiguration;
+import org.springframework.boot.security.autoconfigure.UserDetailsServiceAutoConfiguration;
+import org.springframework.boot.security.autoconfigure.actuate.web.servlet.ManagementWebSecurityAutoConfiguration;
+import org.springframework.boot.security.autoconfigure.web.servlet.SecurityFilterAutoConfiguration;
+
+@SpringBootApplication(exclude = {
+        KafkaAutoConfiguration.class,
+        LiquibaseAutoConfiguration.class,
+//        SecurityAutoConfiguration.class,
+//        UserDetailsServiceAutoConfiguration.class,
+//        ManagementWebSecurityAutoConfiguration.class,
+//        SecurityFilterAutoConfiguration.class
+})
+public class DemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+}
